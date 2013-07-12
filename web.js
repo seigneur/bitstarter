8ed7f25a72f2e6839ba0922485903a6193f5394d
Seigneur <contact.dias@gmail.com>
2013-07-12 15:28:58 +0000

    read from buffer

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+var readVar = fs.readFileSync('index.html');
+var data = readVar.toString();
+response.send(data);
 });
 
 var port = process.env.PORT || 5000;
